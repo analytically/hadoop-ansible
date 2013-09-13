b62368aa5051923bc4911ec0cf5e2393c8ef4907
Mathias Bogaert <mathias.bogaert@gmail.com>
2013-09-13 19:24:13 +0200

    Some var fixes.

--- roles/cdh_hadoop_config/templates/hadoop-env.sh
+++ roles/cdh_hadoop_config/templates/hadoop-env.sh
@@ -1,5 +1,5 @@
 # {{ ansible_managed }}
 
-export HADOOP_HEAPSIZE={{ hadoop["heapsize"] }}
+export HADOOP_HEAPSIZE={{ hadoop.heapsize }}
 
 export HADOOP_OPTS="-server -XX:+UseParallelGC -XX:+AggressiveOpts -XX:+UseNUMA -XX:+UseCondCardMark -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:/var/log/hadoop-hdfs/hadoop-hdfs-gc.log"
