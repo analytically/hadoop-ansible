8da3941d0c59ec91e6511e8f1af1cc31ddb33d41
Mathias Bogaert <mathias.bogaert@gmail.com>
2013-12-07 19:34:15 +0000

    Travis debugging.

--- after_success.sh
+++ after_success.sh
@@ -12,17 +12,18 @@ eval `ssh-agent`
 ssh-add travis.ssh
 
 touch privatehosts
-echo 'running ansible-playbook after_success_setup.yml'
-ansible-playbook -i after_success_hosts --extra-vars "api_key_password=$DO_API_KEY client_id=$DO_CLIENT_ID" after_success_setup.yml
 
-sudo cat /etc/hosts
+touch do_destroy.sh
+chmod +x do_destroy.sh
+
+echo 'running ansible-playbook after_success_setup.yml'
+ansible-playbook -i after_success_hosts --extra-vars "api_key_password=$DO_API_KEY client_id=$DO_CLIENT_ID" after_success.yml
 
 cp travis.ssh.pub bootstrap/ansible_rsa.pub
 cd bootstrap
 ansible-playbook -vvvv -i hosts -u root bootstrap.yml
 cd ..
 
-./site.sh
+ansible-playbook -i hosts --extra-vars "accelerate=false" site.yml
 
-echo 'running ansible-playbook after_success_teardown.yml'
-ansible-playbook -i after_success_hosts --extra-vars "api_key_password=$DO_API_KEY client_id=$DO_CLIENT_ID" after_success_teardown.yml
+./do_destroy.sh
