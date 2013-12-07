684cb5008337a09bbfe20b83ea854482795f2505
Mathias Bogaert <mathias.bogaert@gmail.com>
2013-12-07 23:47:33 +0000

    Fix destroy DO instances.

--- after_success.sh
+++ after_success.sh
@@ -13,8 +13,8 @@ ssh-add travis.ssh
 
 touch privatehosts
 
-touch do_destroy.sh
-chmod +x do_destroy.sh
+sudo touch /etc/rc6.d/K99_do_destroy.sh
+sudo chmod +x /etc/rc6.d/K99_do_destroy.sh
 
 echo 'running ansible-playbook after_success_setup.yml'
 ansible-playbook -i after_success_hosts --extra-vars "api_key_password=$DO_API_KEY client_id=$DO_CLIENT_ID" after_success.yml
