9371a3f6998d4cdb409fa0e7eb382f0cb617eddd
Mathias Bogaert <mathias.bogaert@gmail.com>
2013-12-07 19:16:47 +0000

    Travis debugging.

--- after_success.sh
+++ after_success.sh
@@ -15,9 +15,11 @@ touch privatehosts
 echo 'running ansible-playbook after_success_setup.yml'
 ansible-playbook -i after_success_hosts --extra-vars "api_key_password=$DO_API_KEY client_id=$DO_CLIENT_ID" after_success_setup.yml
 
+sudo cat /etc/hosts
+
 cp travis.ssh.pub bootstrap/ansible_rsa.pub
 cd bootstrap
-ansible-playbook -i hosts -u root bootstrap.yml
+ansible-playbook -vvvv -i hosts -u root bootstrap.yml
 cd ..
 
 ./site.sh
