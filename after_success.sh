12c6e07c443fcad57c4bef5ebbb52fa96cf497c2
Mathias Bogaert <mathias.bogaert@gmail.com>
2013-12-07 19:07:48 +0000

    Travis debugging.

--- after_success.sh
+++ after_success.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+export ANSIBLE_HOST_KEY_CHECKING=False
+export ANSIBLE_SSH_ARGS="-o ForwardAgent=yes"
+
 echo 'installing Dopy'
 sudo pip install dopy
 
@@ -11,8 +14,6 @@ ssh-add travis.ssh
 touch privatehosts
 echo 'running ansible-playbook after_success_setup.yml'
 ansible-playbook -i after_success_hosts --extra-vars "api_key_password=$DO_API_KEY client_id=$DO_CLIENT_ID" after_success_setup.yml
-
-export ANSIBLE_HOST_KEY_CHECKING=False
 
 cp travis.ssh.pub bootstrap/ansible_rsa.pub
 cd bootstrap
