de90606e72275e998388bffe2d6d8f471d39aa09
Mathias Bogaert <mathias.bogaert@gmail.com>
2013-12-07 18:47:04 +0000

    Travis debugging.

--- after_success.sh
+++ after_success.sh
@@ -4,7 +4,8 @@ echo 'installing Dopy'
 sudo pip install dopy
 
 echo 'install the Travis CI SSH key'
-sudo ssh-add $CI_HOME/travis.ssh
+eval `ssh-agent`
+ssh-add $CI_HOME/travis.ssh
 
 echo 'running ansible-playbook after_success_setup.yml'
 ansible-playbook -i $CI_HOME/after_success_hosts --extra-vars "api_key=$DO_API_KEY client_id=$DO_CLIENT_ID" $CI_HOME/after_success_setup.yml
