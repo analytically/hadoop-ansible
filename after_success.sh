fc85c55249997f31ea70d81dd339000c451bd181
Mathias Bogaert <mathias.bogaert@gmail.com>
2013-12-07 18:39:36 +0000

    Travis debugging.

--- after_success.sh
+++ after_success.sh
@@ -4,12 +4,20 @@ echo 'installing Dopy'
 sudo pip install dopy
 
 echo 'install the Travis CI SSH key'
-ssh-add travis.ssh
+sudo ssh-add $CI_HOME/travis.ssh
 
 echo 'running ansible-playbook after_success_setup.yml'
-ansible-playbook -i after_success_hosts --extra-vars "api_key=$DO_API_KEY client_id=$DO_CLIENT_ID" after_success_setup.yml
+ansible-playbook -i $CI_HOME/after_success_hosts --extra-vars "api_key=$DO_API_KEY client_id=$DO_CLIENT_ID" $CI_HOME/after_success_setup.yml
 
-./site.sh
+export ANSIBLE_HOST_KEY_CHECKING=False
+
+mkdir bootstrap
+cp $CI_HOME/bootstrap/* bootstrap
+cp $CI_HOME/travis.ssh.pub bootstrap/ansible_rsa.pub
+cd bootstrap
+ansible-playbook -i hosts -u root bootstrap.yml
+
+$CI_HOME/site.sh
 
 echo 'running ansible-playbook after_success_teardown.yml'
-ansible-playbook -i after_success_hosts --extra-vars "api_key=$DO_API_KEY client_id=$DO_CLIENT_ID" after_success_teardown.yml
+ansible-playbook -i $CI_HOME/after_success_hosts --extra-vars "api_key=$DO_API_KEY client_id=$DO_CLIENT_ID" $CI_HOME/after_success_teardown.yml
