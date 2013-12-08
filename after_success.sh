536946326caa2091d0f65e7c9487e3ffa7bb8d2a
Mathias Bogaert <mathias.bogaert@gmail.com>
2013-12-08 09:18:46 +0000

    Try some fixes.

--- after_success.sh
+++ after_success.sh
@@ -18,7 +18,7 @@ sudo chmod +x /etc/rc6.d/K10do_destroy.sh
 echo 'Run ansible-playbook after_success.yml'
 ansible-playbook -i after_success_hosts --extra-vars "api_key_password=$DO_API_KEY client_id=$DO_CLIENT_ID" after_success.yml
 
-sleep 10
+sleep 15
 
 cp travis.ssh.pub bootstrap/ansible_rsa.pub
 cd bootstrap
@@ -36,17 +36,18 @@ cd hadoop-ansible
 echo '---- Set git settings ----'
 git config --global user.name $GIT_AUTHOR_NAME
 git config --global user.email $GIT_AUTHOR_EMAIL
+git config --global push.default matching
 
 mkdir ci
 mkdir ci/$TRAVIS_BUILD_NUMBER
 cd ci/$TRAVIS_BUILD_NUMBER
-phantomjs screenshot.js http://hmaster01:50070 active-namenode.png
-phantomjs screenshot.js http://hmaster02:50070 standby-namenode.png
-phantomjs screenshot.js http://hmaster01:60010/master-status hbase.png
-phantomjs screenshot.js http://monitor01/ganglia ganglia.png
-phantomjs screenshot.js http://monitor01/kibana/index.html#/dashboard/file/logstash.json kibana.png
-phantomjs screenshot.js http://monitor01:9200/_plugin/head/ elasticsearch-head.png
-phantomjs screenshot.js http://monitor01:9200/_plugin/bigdesk/ elasticsearch-bigdesk.png
+phantomjs ../../screenshot.js http://hmaster01:50070 active-namenode.png
+phantomjs ../../screenshot.js http://hmaster02:50070 standby-namenode.png
+phantomjs ../../screenshot.js http://hmaster01:60010/master-status hbase.png
+phantomjs ../../screenshot.js http://monitor01/ganglia ganglia.png
+phantomjs ../../screenshot.js http://monitor01/kibana/index.html#/dashboard/file/logstash.json kibana.png
+phantomjs ../../screenshot.js http://monitor01:9200/_plugin/head/ elasticsearch-head.png
+phantomjs ../../screenshot.js http://monitor01:9200/_plugin/bigdesk/ elasticsearch-bigdesk.png
 git add .
 git commit -m "[ci skip] Screenshots committed by Travis-CI"
 git push https://${GH_OAUTH_TOKEN}@github.com/${GH_USER_NAME}/${GH_PROJECT_NAME} 2>&1
