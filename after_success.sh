c443bc3d90bbf7b97a24f277bf9c4c2dca02f91e
Mathias Bogaert <mathias.bogaert@gmail.com>
2013-12-08 00:12:02 +0000

    Further fixes.

--- after_success.sh
+++ after_success.sh
@@ -13,8 +13,8 @@ ssh-add travis.ssh
 
 touch privatehosts
 
-sudo touch /etc/rc6.d/K99_do_destroy.sh
-sudo chmod +x /etc/rc6.d/K99_do_destroy.sh
+sudo touch /etc/rc6.d/K10do_destroy.sh
+sudo chmod +x /etc/rc6.d/K10do_destroy.sh
 
 echo 'running ansible-playbook after_success_setup.yml'
 ansible-playbook -i after_success_hosts --extra-vars "api_key_password=$DO_API_KEY client_id=$DO_CLIENT_ID" after_success.yml
@@ -24,6 +24,6 @@ cd bootstrap
 ansible-playbook -i hosts -u root bootstrap.yml
 cd ..
 
-ansible-playbook -i hosts --extra-vars "accelerate=false" site.yml
+ansible-playbook -i hosts --extra-vars "accelerate=true" site.yml
 
-./do_destroy.sh
+sudo /etc/rc6.d/K10do_destroy.sh
