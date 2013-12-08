36df8072dbb4f06d783237398117f8a0f9f64314
Mathias Bogaert <mathias.bogaert@gmail.com>
2013-12-08 16:41:55 +0000

    Cleanup.

--- after_success.sh
+++ after_success.sh
@@ -1,32 +1,5 @@
 #!/bin/sh
 
-export ANSIBLE_HOST_KEY_CHECKING=False
-export ANSIBLE_SSH_ARGS="-o ForwardAgent=yes"
-
-echo 'Install the Travis CI SSH key'
-chmod 400 travis.ssh
-eval `ssh-agent`
-ssh-add travis.ssh
-
-echo 'Touch local temp files'
-touch privatehosts
-sudo touch /etc/rc6.d/K10do_destroy.sh
-sudo chmod +x /etc/rc6.d/K10do_destroy.sh
-
-echo 'Run ansible-playbook after_success.yml'
-ansible-playbook -i after_success_hosts --extra-vars "api_key_password=$DO_API_KEY client_id=$DO_CLIENT_ID" after_success.yml
-
-sleep 15
-
-cp travis.ssh.pub bootstrap/ansible_rsa.pub
-cd bootstrap
-echo 'Run ansible-playbook -i hosts -u root bootstrap.yml'
-ansible-playbook -i hosts -u root bootstrap.yml
-cd ..
-
-echo 'Run ansible-playbook -i hosts --extra-vars "accelerate=false" site.yml'
-ansible-playbook -i hosts --extra-vars "accelerate=false" site.yml
-
 cd /tmp
 git clone https://${GH_OAUTH_TOKEN}@github.com/${GH_USER_NAME}/${GH_PROJECT_NAME} hadoop-ansible
 cd hadoop-ansible
@@ -47,6 +20,3 @@ cd ci/$TRAVIS_BUILD_NUMBER
 git add .
 git commit -m "[ci skip] Screenshots committed by Travis-CI"
 git push https://${GH_OAUTH_TOKEN}@github.com/${GH_USER_NAME}/${GH_PROJECT_NAME} 2>&1
-
-echo 'Destroying DigitalOcean instances...'
-sudo /etc/rc6.d/K10do_destroy.sh
