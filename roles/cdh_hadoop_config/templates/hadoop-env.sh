cb126170c214ad2c34de056db4fd5249010ae19c
Mathias Bogaert <mathias.bogaert@gmail.com>
2013-12-19 00:10:12 +0000

    Fix maxpermsize.

--- roles/cdh_hadoop_config/templates/hadoop-env.sh
+++ roles/cdh_hadoop_config/templates/hadoop-env.sh
@@ -2,4 +2,4 @@
 
 export HADOOP_HEAPSIZE={{ heapsize }}
 
-export HADOOP_OPTS="-Xbootclasspath/p:/usr/lib/jvm/floatingdecimal-0.1.jar -XX:+UseConcMarkSweepGC -XX:+AggressiveOpts -XX:+MaxPermSize=300M -XX:+CMSClassUnloadingEnabled -XX:SurvivorRatio=8 -XX:+ExplicitGCInvokesConcurrent -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:/var/log/hadoop-hdfs/hadoop-hdfs-gc.log"
+export HADOOP_OPTS="-Xbootclasspath/p:/usr/lib/jvm/floatingdecimal-0.1.jar -XX:+UseConcMarkSweepGC -XX:+AggressiveOpts -XX:MaxPermSize=300M -XX:+CMSClassUnloadingEnabled -XX:SurvivorRatio=8 -XX:+ExplicitGCInvokesConcurrent -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:/var/log/hadoop-hdfs/hadoop-hdfs-gc.log"
