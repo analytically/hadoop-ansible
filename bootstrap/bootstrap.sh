ce375da171441051e4d5a3849a9086aa8dc50ebf
coderfi <coderfi@gmail.com>
2014-02-26 08:57:59 -0800

    removed ultra verbose -vvvv setting, replaced with -vv

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -11,4 +11,4 @@ if [ ! -f "hosts" ]; then
 fi
 
 export ANSIBLE_HOST_KEY_CHECKING=False
-ansible-playbook -i hosts -k -K -u ansibler -vvvv bootstrap.yml 
+ansible-playbook -i hosts -k -K -u ansibler -vv bootstrap.yml 
